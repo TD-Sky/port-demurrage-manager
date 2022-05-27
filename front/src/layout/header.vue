544e3ef971ee872f09af1aee449c4ec21e01cdea
TD-Sky <1752963246@qq.com>
2022-05-27 21:57:46 +0800

    顶栏组件

---
+++ front/src/layout/header.vue
@@ -0,0 +1,13 @@
+<script  setup lang="ts">
+import { Fold } from '@element-plus/icons-vue'
+</script>
+
+<template>
+    <header>
+        <el-button plain>
+            <el-icon>
+                <Fold />
+            </el-icon>
+        </el-button>
+    </header>
+</template>
--- front/src/layout/index.vue
+++ front/src/layout/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Sidebar from './sidebar.vue'
+import Header from './header.vue'
 </script>
 
 <template>
@@ -9,7 +10,9 @@ import Sidebar from './sidebar.vue'
                 <Sidebar />
             </el-aside>
             <el-container>
-                <el-header>Header</el-header>
+                <el-header>
+                    <Header />
+                </el-header>
                 <el-main>Main</el-main>
             </el-container>
         </el-container>
