bf4c514f6619a7affe229815b154d5322dff4f53
TD-Sky <1752963246@qq.com>
2022-06-05 14:30:34 +0800

    表单修改

--- service/src/dba/load.go
+++ service/src/dba/load.go
@@ -1,15 +1,23 @@
 package dba
 
 import (
-	"service/models"
-
 	"github.com/jmoiron/sqlx"
+	"service/models"
 )
 
 func Select_loads(db *sqlx.DB, loads *[]models.GetLoad) {
 	db.Select(loads,
-		`select id, order_number, load_date, loads, load_ton, business_number, lading_bill_number
-         from shipping_order
-         left join load
-         on num = order_number`)
+        `select id, order_number, load_date, loads, load_ton, business_number, lading_bill_number
+            from shipping_order
+            left join load
+            on num = order_number
+            order by load_date ASC`)
+}
+
+func Update_load(db *sqlx.DB, load models.PutLoad) {
+	db.NamedExec(
+		`update load set (load_date, loads, load_ton)
+            = (:load_date, :loads, :load_ton)
+            where id = :id`,
+		load)
 }
--- service/src/models/load.go
+++ service/src/models/load.go
@@ -9,3 +9,10 @@ type GetLoad struct {
 	Business_number    string  `json:"business_number" db:"business_number"`
 	Lading_bill_number string  `json:"lading_bill_number" db:"lading_bill_number"`
 }
+
+type PutLoad struct {
+	ID                 uint32  `json:"id" db:"id"`
+	Load_date          string  `json:"load_date" db:"load_date"`
+	Loads              uint32  `json:"loads" db:"loads"`
+	Load_ton           float64 `json:"load_ton" db:"load_ton"`
+}
