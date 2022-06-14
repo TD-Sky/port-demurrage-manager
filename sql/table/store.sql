82cb82973e12cd56f98ed180db3042680bab8493
TD-Sky <1752963246@qq.com>
2022-06-14 23:14:33 +0800

    删除尾逗号

--- sql/table/store.sql
+++ sql/table/store.sql
@@ -7,5 +7,5 @@ create table store
     license_plate_number char(7) not NULL,      -- 车牌号
     stocks int not NULL,                        -- 入库件数
     store_ton float8 not NULL,                  -- 入库吨数
-    duration int default 0 not NULL,            -- 存放天数，每日0点更新
+    duration int default 0 not NULL             -- 存放天数，每日0点更新
 );
